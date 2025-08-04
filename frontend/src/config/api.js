3943360c04f68a1cc76516ae397104dccb9d04fb
masaris888 <anampict@gmail.com>
2025-08-04 21:04:39 +0700

    crud itemMachine Done

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -4,4 +4,5 @@ export const API_BASE_URL = "http://127.0.0.1:8000/api"; // Ganti sesuai domain 
 export const ENDPOINTS = {
   login: `${API_BASE_URL}/login`,
   users: `${API_BASE_URL}/users`,
+  itemMachines: `${API_BASE_URL}/item-machines`,
 };
